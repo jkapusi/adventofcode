fb533aa10ee8562090270290cf1340c3cd247060
Jozsef Kapusi <Jozsef.Kapusi@hu.ibm.com>
2020-12-02 09:44:12 +0100

    solve problem 2 - not so elegant, but I don't have more time

--- day1/solve.sh
+++ day1/solve.sh
@@ -2,11 +2,20 @@
 
 IFS=$'\n' read -d '' -r -a INPUT < input
 
-echo ${INPUT[@]}
 for x in ${INPUT[@]}; do
     for y in ${INPUT[@]}; do
         if [ $((x+y)) -eq 2020 ]; then
             echo "$x + $y = 2020; $x * $y = $((x*y))"
         fi
+    done
+done
+
+for x in ${INPUT[@]}; do
+    for y in ${INPUT[@]}; do
+        for z in ${INPUT[@]}; do
+            if [ $((x+y+z)) -eq 2020 ]; then
+                echo "$x + $y + $z = 2020; $x * $y * $z = $((x*y*z))"
+            fi
+        done
     done
 done
